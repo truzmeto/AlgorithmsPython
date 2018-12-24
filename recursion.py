f88ac559440afb0730bc49be25d96855bac0493d
truzmeto <talantruzmetov@gmail.com>
2018-12-24 10:34:48 -0500

    added comments

--- recursion.py
+++ recursion.py
@@ -3,10 +3,11 @@ def reverse(s):
     """
     Recursive function to reverse the string.
     """
-    
+    #base case
     if len(s) <= 1:
         return s
     else:
+        #join last element to the reverse of the rest
         return s[-1] + reverse(s[:-1])
 
 
@@ -16,14 +17,16 @@ def permute(s):
     Recursive function to find all permutations of 
     a given string.
     """
-    out = []
+    out = [] #empty list
+    #base case
     if len(s) <= 1:
         out = [s]
     else:
-        
+        #for each element in a string
         for i, let in enumerate(s):
+            #permute the remaining recursivaly
             for perm in permute(s[:i] + s[i+1:]):
-                
+                #and append 'ele + perm' to a list
                 out += [let + perm]
                 
     return out           
